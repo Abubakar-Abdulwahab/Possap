e0f401cfc60d4fe3b79287d4557766d466c837ca
ImaanWada <62875755+ImaanWada@users.noreply.github.com>
2022-01-12 22:23:58 +0100

    status badge

---
+++ src/components/status/status.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const statusBadge = ({ row, value, status }) => {
+  return (
+    <>
+      <div>
+        <span class="badge bg-primary">Primary</span>
+      </div>
+    </>
+  );
+};
